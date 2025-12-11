aa1161283cac0d1b40be5e453034b5d8131b4ac3
abdulfaiq47 <abdulfaiq47@gmail.com>
2025-12-11 10:43:40 +0500

    Updated

--- app/page.js
+++ app/page.js
@@ -2,20 +2,32 @@
 import Image from "next/image";
 // import LiquidLight from "@/components/Hover/page";
 import styles from "./page.module.css";
-import GradualBlur from "@/components/BottBlur/page";
+// import GradualBlur from "@/components/BottBlur/page";
 // import SciFiDashboard from "@/components/Background/page";
 // import SciFiDashboard from "@/components/Background/page";
-import Mousehov from "@/components/MOuseH/page";
-import Dashboard from "@/components/Backgr/page";
+// import Mousehov from "@/components/MOuseH/page";
+// import Dashboard from "@/components/Backgr/page";
 import Navbar from "@/components/Navbar/page";
-import Card from "@/components/Scrol/page";
-import Skills from "@/components/Skill/page";
+// import Card from "@/components/Scrol/page";
+// import Skills from "@/components/Skill/page";
 import { ContactF } from "@/components/Contact/page";
-import ScrollReveal from "@/components/ScrolRevel/page";
+// import ScrollReveal from "@/components/ScrolRevel/page";
 // import { ReactTyped } from "react-typed";
-import Projectss from "@/components/Projets/page";
-import TypedComponent from "@/components/Typecom/page";
+// import Projectss from "@/components/Projets/page";
+// import TypedComponent from "@/components/Typecom/page";
 import Link from "next/link";
+
+import dynamic from "next/dynamic";
+
+const Dashboard = dynamic(() => import("@/components/Backgr/page"), { ssr: false });
+const GradualBlur = dynamic(() => import("@/components/BottBlur/page"), { ssr: false });
+const Mousehov = dynamic(() => import("@/components/MOuseH/page"), { ssr: false });
+const Card = dynamic(() => import("@/components/Scrol/page"), { ssr: false });
+const Skills = dynamic(() => import("@/components/Skill/page"), { ssr: false });
+const Projectss = dynamic(() => import("@/components/Projets/page"), { ssr: false });
+// const ContactF = dynamic(() => import("@/components/Contact/page"), { ssr: false });
+const ScrollReveal = dynamic(() => import("@/components/ScrolRevel/page"), { ssr: false });
+const TypedComponent = dynamic(() => import("@/components/Typecom/page"), { ssr: false });
 
 export default function Home() {
   return (
